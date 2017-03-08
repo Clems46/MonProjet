b358a02be5fe79cda1e415953616f24d2aa371a9
Clems46 <clems46@laposte.net>
2017-03-08 20:52:22 +0100

    test du buffer overflow avec scanf

--- test.c
+++ test.c
@@ -4,10 +4,20 @@
 int main() {
 
 char reponseUtilisateur[10];
+char bufferoverflow1[10];
+char bufferoverflow2[10];
+
         printf ("Hello, World!\n");
 	printf ("Quel est votre nom ?\n");
 	fgets (reponseUtilisateur, 10, stdin); // Plus puissant que Scanf et plus secure
 	printf("Ah ! vous vous appelez donc %s !", reponseUtilisateur); 
-        return 0;
+printf("Ecrire n'importe quoi\n");  //test du buffer overflow
+scanf("%s", bufferoverflow1);
+printf("premier truc:%s", bufferoverflow1);
+printf("second truc:%s", bufferoverflow1);
+scanf("%s", bufferoverflow2);
+printf("3eme truc :%s", bufferoverflow2);        
+
+return 0;
 
 }
