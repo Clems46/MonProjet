4912550760c79bd0010eeef6555dd015ba311fbe
Clems46 <clems46@laposte.net>
2017-03-08 20:41:15 +0100

    Ajout de la fonction Prénom

--- test.c
+++ test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
+char reponseUtilisateur[10];
         printf ("Hello, World!\n");
+	printf ("Quel est votre nom ?\n");
+	fgets (reponseUtilisateur, 10, stdin); // Plus puissant que Scanf et plus secure
+	printf("Ah ! vous vous appelez donc %s !", reponseUtilisateur); 
         return 0;
+
 }
